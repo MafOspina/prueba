0678433d572090d9d1a25a2d3caafa113db9cc17
Maria_Ospina <mariaospina@cbit-online.com>
2023-06-07 15:48:50 -0500

    first commit

---
+++ src/test/java/com/example/LabSpringBoot/LabSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LabSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
